b1319f719bdcd8549ae584eb926cac0536944f48
pharaoh <pharaohmanson@gmail.com>
2024-01-31 17:41:14 -0500

    next.js layout feature complete

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+
+    return (
+        <div>
+            <h2>Not Found</h2>
+            <p>Sorry page does not exist.</p>
+        </div>
+    )
+};
+
+export default NotFound;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,3 +6,9 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
+
+// app is specified as our router folder, all folders dealing with routes should go there
+// components and everything else goes in src
+//all route files must be name page

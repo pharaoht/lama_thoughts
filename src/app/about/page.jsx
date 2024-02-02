bb5e67ef92e346d13d7d5f399ff78d317bcd9042
pharaoh <pharaohmanson@gmail.com>
2024-02-02 10:15:31 -0500

    contact page details

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,45 @@
+import Image from 'next/image';
+import styles from './about.module.css';
+
 const AboutPage = () => {
 
     return (
-        <div>AboutPage</div>
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Agency</h2>
+                <h1 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better.</h1>
+                <p className={styles.desc}>
+                    Lorem, ipsun dolor sit amet consectetur adipiscing elit. Vero blandtiits adipisci minima reciciendis a autem assumenda dolore
+                </p>
+                <div className={styles.boxes}>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>
+                            Years of experience
+                        </p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>
+                            Years of experience
+                        </p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>
+                            Years of experience
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.imgContainer}>
+                <Image
+                    src='/about.png'
+                    alt='About Image'
+                    fill
+                />
+            </div>
+        </div>
     )
 };
 
@@ -12,5 +50,5 @@ export default AboutPage;
 //in next.js we use <Image/> tag not <img/>
 //next.js is not showing our images directly, instead it is caching/optimizing it first in the next folder
 //for different screen sizes
-//To utlize this feature, you can add fill to the tag and it will reference the size of its parent container
+//To utilize this feature, you can add fill to the tag and it will reference the size of its parent container
 //make sure your parent container position is relative
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,29 @@
+import styles from './contact.module.css';
+import Image from 'next/image';
+
 const ContactPage = () => {
 
     return (
-        <div>Contact Page</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src='/contact.png' alt='' fill className={styles.img} />
+            </div>
+            <div className={styles.formContainer}>
+                <form action='' className={styles.form}>
+                    <input type='text' placeholder='Name and Sur Name' />
+                    <input type='email' placeholder='Email Address' />
+                    <input type='tel' placeholder='Phone Number (Optional)' />
+                    <textarea
+                        name=''
+                        id=''
+                        cols={30}
+                        rows={10}
+                        placeholder='Message'
+                    ></textarea>
+                    <button>Send</button>
+                </form>
+            </div>
+        </div>
     )
 };
 

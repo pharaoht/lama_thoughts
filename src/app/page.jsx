1349c53dcc99a7892f601ae3cea47d6048306943
pharaoh <pharaohmanson@gmail.com>
2024-02-01 19:27:35 -0500

    home page completion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,28 @@
+import styles from './home.module.css';
+import Image from 'next/image';
+
 const HomePage = () => {
 
   return (
-    <div>Home</div>
+
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+        <p className={styles.desc}>
+          Lorem, ipsun dolor sit amet consectetur adipiscing elit. Vero blandtiits adipisci minima reciciendis a autem assumenda dolore
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image className={styles.brandImg} src="/brands.png" alt="brands logo" fill />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src='/hero.gif' alt='gif of light bulb' fill className={styles.heroImg} />
+      </div>
+    </div>
   )
 };
 

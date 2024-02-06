37281904bd9fc3f7a57cac9cc3a0742e785c78c0
pharaoh <pharaohmanson@gmail.com>
2024-02-06 17:53:15 -0500

    notes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -30,6 +30,3 @@ export default HomePage;
 
 
 
-// app is specified as our router folder, all folders dealing with routes should go there
-// components and everything else goes in src
-//all route files must be name page
